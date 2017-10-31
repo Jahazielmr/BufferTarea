d787703b6b91f43541242b3e47b396627d68346f
Jahazielmr <Jahazielmr97@gmail.com>
2017-10-30 21:30:59 -0600

    awd

--- Main.cpp
+++ Main.cpp
@@ -9,22 +9,28 @@ using namespace std;
 
 
 int main(){
+	cout<<"Ejercicio 1, cargamos un archivo de txt con el reader del buffer"<<endl;
+	cout<<endl;
 	
 		ifstream fin;
 
+		vector <Persona> personasAux;
+	    
+
         
-        /* for (int i = 0; i < personas.size(); ++i)
+         for (int i = 0; i < personasAux.size(); ++i)
          {
-         	cout<< "Nombre "<<personas[i].getNombre()<<" "<<"Apellido " <<personas[i].getApellido()<<" "<<"Direccion "<<personas[i].getDireccion()<<" Edad "<<personas[i].getEdad()<<" Telefono "<<personas[i].getTelefono()<<endl;
+         	cout<< "Nombre "<<personasAux[i].getNombre()<<" "<<"Apellido " <<personasAux[i].getApellido()<<" "<<"Direccion "<<personasAux[i].getDireccion()<<" Edad "<<personasAux[i].getEdad()<<" Telefono "<<personasAux[i].getTelefono()<<endl;
          }
 
-		*/
-        
         
         fin.close();
 
-	
-	
+	cout<<endl;
+	cout<<endl;
+
+	cout<<"Ejercicio 2, escribir los datos de un vector a un archivo de texto usando el metodo writer del buffer";
+	//Pone aqui abajo tu codigo alessandro !
 	
 	
 
